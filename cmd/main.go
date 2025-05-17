ab18a5cde6c6c801b39434f6c1dacc72104e79ae
Daniele Zannotti <d.zannotti@me.com>
2025-05-17 12:05:23 +0100

    done big render

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/png"
 	"log"
+	"math/rand/v2"
 	"os"
 	"raytracer/math3"
 	"raytracer/raytracer"
@@ -23,29 +24,28 @@ func main() {
 	ground := raytracer.Lambertian{Albedo: math3.Vec3{0.5, 0.5, 0.5}}
 	world.Add(raytracer.Sphere{Center: math3.Vec3{0, -1000, -1}, Radius: 1000, Material: ground})
 
-	/*
-		for a := -11; a < 11; a++ {
-			for b := -11; b < 11; b++ {
-				chooseMaterial := rand.Float64()
-				center := math3.Vec3{float64(a) + 0.9*rand.Float64(), 0.2, float64(b) + 0.9*rand.Float64()}
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMaterial := rand.Float64()
+			center := math3.Vec3{float64(a) + 0.9*rand.Float64(), 0.2, float64(b) + 0.9*rand.Float64()}
 
-				if center.Sub(math3.Vec3{4, 0.2, 0}).Length() > 0.9 {
-					albedo := math3.Random().Multiply(math3.Random())
-					var material raytracer.Material
-					switch {
-					case chooseMaterial < 0.8:
-						material = raytracer.Lambertian{Albedo: albedo}
-					case chooseMaterial < 0.95:
-						albedo = math3.RandomBetween(0.5, 1)
-						fuzz := math3.RandomBetween(0, 0.5)[0]
-						material = raytracer.Metal{Albedo: albedo, Fuzz: fuzz}
-					default:
-						material = raytracer.Dialectric{RefractionIndex: 1.5}
-					}
-					world.Add(raytracer.Sphere{Center: center, Radius: 0.2, Material: material})
+			if center.Sub(math3.Vec3{4, 0.2, 0}).Length() > 0.9 {
+				albedo := math3.Random().Multiply(math3.Random())
+				var material raytracer.Material
+				switch {
+				case chooseMaterial < 0.8:
+					material = raytracer.Lambertian{Albedo: albedo}
+				case chooseMaterial < 0.95:
+					albedo = math3.RandomBetween(0.5, 1)
+					fuzz := math3.RandomBetween(0, 0.5)[0]
+					material = raytracer.Metal{Albedo: albedo, Fuzz: fuzz}
+				default:
+					material = raytracer.Dialectric{RefractionIndex: 1.5}
 				}
+				world.Add(raytracer.Sphere{Center: center, Radius: 0.2, Material: material})
 			}
-		}*/
+		}
+	}
 
 	mat1 := raytracer.Dialectric{RefractionIndex: 1.5}
 	mat2 := raytracer.Lambertian{Albedo: math3.Vec3{0.4, 0.2, 0.1}}
@@ -56,9 +56,9 @@ func main() {
 	world.Add(raytracer.Sphere{Center: math3.Vec3{4, 1, 0}, Radius: 1, Material: mat3})
 
 	camera := raytracer.NewCamera(raytracer.CameraParams{
-		Width:           640,
+		Width:           1200,
 		AspectRatio:     16 / 9.0,
-		SamplesPerPixel: 10,
+		SamplesPerPixel: 500,
 		MaxDepth:        50,
 		VFov:            20,
 		LookFrom:        math3.Vec3{13, 2, 3},
@@ -67,6 +67,7 @@ func main() {
 		FocusDist:       10,
 	})
 	render := camera.Render(world, true)
+	fmt.Println("denoising....")
 	img := raytracer.BilateralFilter(render, 3.0, 0.2)
 	file, err := os.Create("final-screenshot.png")
 	if err != nil {
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Ray tracer in TS
+
+Based off of the excellent [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview)
+
+## To run:
+
+```
+make run
+```
+
+takes about 
+
+The paramaters of the camera and the setup of material and objects in the world are all located in `./cmd/main.go`
+
+![A rendering of spheres, some metallic, some glass and transparent, and some metallic and reflective](./final-screenshot.png) 
